2c3f867c5e7446d9b392af049947100916acffb3
prashantnitt07 <31304206+prashantnitt07@users.noreply.github.com>
2025-11-09 17:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,4 +109,4 @@ def metrics():
 # Entry point (for local testing)
 # ---------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=5000)
